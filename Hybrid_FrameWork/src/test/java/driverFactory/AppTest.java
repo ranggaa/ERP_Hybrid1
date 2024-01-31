feaff1019b68b768315d2618f7f25c80e4e4cd2a
pranga82 <pranga82@gmail.com>
2024-01-31 18:12:47 +0530

    First Commit

---
+++ Hybrid_FrameWork/src/test/java/driverFactory/AppTest.java
@@ -0,0 +1,12 @@
+package driverFactory;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+@Test
+public void KickStart() throws Throwable
+{
+	DriverScript ds = new DriverScript();
+	ds.startTest();
+}
+}
